b3949117b62afea6ec2c17db18fe4ba32b16e625
Junaid <junaid@gmail.com>
2025-06-26 14:29:37 +0530

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.WriteLine("Enter a number: ");
+
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num % 2 == 0)
+{
+    Console.WriteLine("even");
+}
+else
+{
+    Console.WriteLine("odd");
+}
